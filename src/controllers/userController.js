842650ebfc51ddc652782d39f43a9f3e31b8e313
abrarabedin001 <abrarabedin001@gmail.com>
2023-06-23 12:03:50 +0600

    Done with singIn sign up backend

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,42 @@
 const prisma = require('../Database');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'skldjfa;lsdj';
 
 const signup = async (req, res) => {
   let { email, name, password } = req.body;
   try {
-    let users = await prisma.user.findMany();
     const hashedPassword = await bcrypt.hash(password, 10);
-    res.status(200).json({ message: hashedPassword });
-  } catch (err) {}
+
+    let user = await prisma.user.create({
+      data: { name: name, email: email, password: hashedPassword },
+    });
+    const token = jwt.sign({ email: user.email, id: user.id }, SECRET_KEY);
+
+    res.status(201).json({ user: user, token: token });
+  } catch (err) {
+    res.status(404).json({ message: 'something went wrong', error: err });
+  }
 };
 
-const signin = (req, res) => {};
+const signin = async (req, res) => {
+  let { email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    res.status(200).json({ message: 'Signin successful', user });
+  } catch (err) {
+    res.status(500).json({ message: 'Something went wrong', error: err });
+  }
+};
 module.exports = { signin, signup };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,16 @@
-const express = require("express")
-const prisma = require("../Database")
-const { signup, signin } = require("../controllers/userController")
+const express = require('express');
+const prisma = require('../Database');
+const { signup, signin } = require('../controllers/userController');
 
 // const {PrismaClient }= require('@prisma/client')
 // const prisma = new PrismaClient()
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.post("/signup",signup)
-userRouter.get("/signin",signin)
+userRouter.post('/signup', signup);
+userRouter.post('/signin', signin);
 
-module.exports = userRouter
+module.exports = userRouter;
 
 // async (req,res)=>{
 //   let {email,name,password} = req.body()
