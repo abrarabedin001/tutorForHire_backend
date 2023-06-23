936996b9542233853c8bb0ae848150b3eeb71a98
abrarabedin001 <abrarabedin001@gmail.com>
2023-06-24 01:49:49 +0600

    added middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+    if (token) {
+      token = token.split(' ')[1];
+      let user = jwt.verify(token, SECRET_KEY);
+      req.userId = user.id;
+    } else {
+      res.status(401).json({ message: 'Unauthorized User' });
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = auth;
