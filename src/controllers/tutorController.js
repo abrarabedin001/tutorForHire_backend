694dbf5dc4fa66773ab2d067feb8e9890efe3976
anikmd445 <anikmd445@gmail.com>
2023-06-24 22:30:32 +0600

    on user update

--- src/controllers/tutorController.js
+++ src/controllers/tutorController.js
@@ -5,60 +5,60 @@ const SECRET_KEY = 'skldjfa;lsdj';
 
 // localhost:3000/student/:id
 
-const create = async (req, res) => {
-  console.log('New Student');
-  console.log(req.user);
-  console.log(req.body);
-  // res.status(200).json({ message: req.userId });
-
+const tutorCreate = async (req, res) => {
   let { bio, education } = req.body;
   try {
-    let studentProfile = await prisma.studentProfile.create({
+    let teacherProfile = await prisma.teacherProfile.create({
       data: { bio: bio, education: education, userId: req.user.id },
     });
 
-    res.status(201).json({ studentProfile: studentProfile });
+    res.status(201).json({ teacherProfile: teacherProfile });
   } catch (err) {
     res.status(404).json({ message: 'something went wrong', error: err });
   }
 };
 
-const update = async (req, res) => {
+
+
+const tutorPatch = async (req, res) => {
   let { password, bio, education } = req.body;
+
+  
+const hashedPassword = await bcrypt.hash(password, 10);
+const updateTeacher = await prisma.teacherProfile.update({
+  where: {
+    userId:req.user.id
+  },
+  data: {
+    user:{
+        update:{
+            password:hashedPassword
+    },
+    bio:bio,
+    education:education
+  },
+}
+})
   console.log(req.user.id)
 //   console.log(req.user.password)
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const updateStudent = await prisma.studentProfile.update({
-      where: {
-        userId:req.user.id
-      },
-      data: {
-        user:{
-            update:{
-                password:hashedPassword
-            }
-        },
-        bio:bio,
-        education:education
-      },
-    })
-    console.log(updateStudent)
+
+    console.log(updateTeacher)
     console.log(password)
-    res.status(201).json({ updateStudent: updateStudent });
+    res.status(201).json({ updateTeacher: updateTeacher });
   } catch (err) {
     res.status(404).json({ message: 'something went wrong', error: err });
   }
 
 };
-
+//
 
 
 //not done yet
-const Delete = async (req, res) => {
+const tutorDelete = async (req, res) => {
   let { bio,education } = req.body;
   try {
-    const deleteStudent = await prisma.studentProfile.delete({
+    const deleteTeacher = await prisma.teacherProfile.delete({
       where: OR [ {
         userId:req.user.id,
       },
@@ -71,12 +71,13 @@ const Delete = async (req, res) => {
       education:education
     }
     })
-    res.status(201).json({ deleteStudent: deleteStudent });
+    res.status(201).json({ deleteTeacher: deleteTeacher });
 
   } catch (err) {
     res.status(404).json({ message: 'something went wrong', error: err });
   }
 };
 
+//
 
-module.exports = { create,update,Delete};
+module.exports = { tutorCreate,tutorPatch,tutorDelete };
--- src/routes/tutorRoutes.js
+++ src/routes/tutorRoutes.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const prisma = require('../Database');
-const { signup, signin } = require('../controllers/userController');
+const { tutorCreate,tutorPatch,tutorDelete } = require('../controllers/tutorController');
+const auth = require('../middlewares/auth');
 
-// const {PrismaClient }= require('@prisma/client')
-// const prisma = new PrismaClient()
 
 const tutorRouter = express.Router();
-// localhost:3000/users/singin
-// functions -> controller
 
+tutorRouter.post('/tutorcreate', auth, tutorCreate);
+tutorRouter.patch('/tutorupdate', auth, tutorPatch);
+tutorRouter.delete('/tutordelete', auth, tutorDelete);
 
 module.exports = tutorRouter;
-
-// async (req,res)=>{
-//   let {email,name,password} = req.body()
-//   let users = await prisma.user.findMany()
-//   res.send(users)
-// }
-// MVC model-database view-router controller-function
