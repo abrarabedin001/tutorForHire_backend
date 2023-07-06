c0b0422bff9073c2a25355572e2abac63181bda3
anikmd445 <anikmd445@gmail.com>
2023-07-06 22:10:57 +0600

    course enroll ment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const userRouter = require('./routes/userRoutes');
 const tutorRouter = require('./routes/tutorRoutes');
 const studentRouter = require('./routes/studentRoutes');
 const courseRouter = require('./routes/courseRoutes');
+const enrollRouter = require('./routes/enrollRoutes');
 
 const app = express();
 
@@ -20,7 +21,8 @@ app.use(express.json());
 app.use('/users', userRouter);
 app.use('/tutor', tutorRouter);
 app.use('/student', studentRouter);
-app.use('/course',courseRouter)
+app.use('/course',courseRouter);
+app.use('/enrollcourse',enrollRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello');
