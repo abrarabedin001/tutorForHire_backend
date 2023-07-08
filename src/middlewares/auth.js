6f5f73ddd4ee3f9d52d8f03b9feb76e61dc6821a
abrarabedin001 <abrarabedin001@gmail.com>
2023-07-08 18:49:47 +0600

    .

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,18 +2,21 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = 'skldjfa;lsdj';
 
 const auth = (req, res, next) => {
+  console.log('kaj kore skdflsj ');
   try {
     let token = req.headers.authorization;
     if (token) {
       token = token.split(' ')[1];
       let user = jwt.verify(token, SECRET_KEY);
-
+      console.log('---------------------');
       req.user = user;
     } else {
       res.status(401).json({ message: 'Unauthorized User' });
     }
     next();
   } catch (error) {
+    console.log('---------------------');
+    console.log(req.headers.authorization);
     console.log(error);
   }
 };
